e3189d55aba438ad52c0a8ed8ee0a79b051c4bbf
Mohammadreza Ghorbani <isthereanybody@gmail.com>
2016-12-10 12:41:49 +0330

    Fix routes.php

--- src/Config/routes.php
+++ src/Config/routes.php
@@ -1,6 +1,5 @@
 <?php
 
 // Insert Routes here.
-return [
-    Route::get("/package-provider", \Easteregg\Package\Http\Controllers\PackageProviderController::class . '@index'),
-];
+Route::get("/package-provider", \Easteregg\Package\Http\Controllers\PackageProviderController::class . '@index');
+
