f39c062b175a92c0c819bcd7fd57b76e10cb970a
Mohammadreza Ghorbani <isthereanybody@gmail.com>
2017-08-27 03:43:12 +0430

    Add Support for Mongodb Date

--- src/LocalizesCarbon.php
+++ src/LocalizesCarbon.php
@@ -1,66 +1,67 @@
 <?php
-
 namespace Easteregg\IntlCarbon;
 
 use Carbon\Carbon;
-use IntlDateFormatter;
+use DateTimeInterface;
+use MongoDB\BSON\UTCDateTime;
 
-class LocalizesCarbon extends Carbon
+trait LocalizesDates
 {
-    protected $_intlFormatter;
 
-    public function __construct($time = null, $tz = null)
+    public function freshTimestamp()
     {
-        parent::__construct($time, $tz);
+        // was previously:  return new Carbon;
+        return new LocalizesCarbon();
     }
 
     /**
-     * @return mixed
+     * Return a timestamp as DateTime object.
+     *
+     * @param  mixed  $value
+     * @return \Carbon\Carbon
      */
-    public function getIntlFormatter()
+    protected function asDateTime($value)
     {
-        return $this->_intlFormatter;
-    }
+        // If this value is already a Carbon instance, we shall just return it as is.
+        // This prevents us having to re-instantiate a Carbon instance when we know
+        // it already is one, which wouldn't be fulfilled by the DateTime check.
+        if ($value instanceof Carbon) {
+            return $value;
+        }
 
-    /**
-     * @return string
-     */
-    public function long()
-    {
-        $timezone = "Asia/Tehran";
-        $locale = $this->getLocaleFromConfig();
-        $calendar = IntlDateFormatter::TRADITIONAL;
-        $formatter = new IntlDateFormatter(
-            $locale,
-            IntlDateFormatter::LONG,
-            IntlDateFormatter::LONG,
-            $timezone,
-            $calendar
+        // If the value is a MongoDB\BSON\UTCDateTime, then we convert it first to DateTime Value
+        if ($value instanceof UTCDateTime) {
+            $value = $value->toDateTime();
+        }
+
+        // If the value is already a DateTime instance, we will just skip the rest of
+        // these checks since they will be a waste of time, and hinder performance
+        // when checking the field. We will just return the DateTime right away.
+        if ($value instanceof DateTimeInterface) {
+            return new LocalizesCarbon(
+                $value->format('Y-m-d H:i:s.u'), $value->getTimezone()
+            );
+        }
+
+        // If this value is an integer, we will assume it is a UNIX timestamp's value
+        // and format a Carbon object from this timestamp. This allows flexibility
+        // when defining your date fields as they might be UNIX timestamps here.
+        if (is_numeric($value)) {
+            return LocalizesCarbon::createFromTimestamp($value);
+        }
+
+        // If the value is in simply year, month, day format, we will instantiate the
+        // Carbon instances from that format. Again, this provides for simple date
+        // fields on the database, while still supporting Carbonized conversion.
+        if ($this->isStandardDateFormat($value)) {
+            return LocalizesCarbon::createFromFormat('Y-m-d', $value)->startOfDay();
+        }
+
+        // Finally, we will just assume this date is in the format used by default on
+        // the database connection and use that format to create the Carbon object
+        // that is returned back out to the developers after we convert it here.
+        return LocalizesCarbon::createFromFormat(
+            $this->getDateFormat(), $value
         );
-        $formatter->setPattern($this->getIntlFormatterFromConfig());
-        return $formatter->format($this);
-    }
-
-    /**
-     * @param mixed $intlFormatter
-     */
-    public function setIntlFormatter($intlFormatter)
-    {
-        $this->_intlFormatter = $intlFormatter;
-    }
-
-    private function getIntlFormatterFromConfig()
-    {
-        $appLocale = config("app.locale");
-        return config("intl.{$appLocale}.format");
-    }
-
-    /**
-     * @return string
-     */
-    private function getLocaleFromConfig(): string
-    {
-        $appLocale = config("app.locale");
-        return config("intl.{$appLocale}.calendar");
     }
 }
